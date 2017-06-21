50ab39665344ce5f223af2a15a8143ffdf9f7105
Ryan McBurney <rrmcburn@gmail.com>
2017-06-20 20:11:16 -0400

    Creating Rubygem

---
+++ lib/kele.rb
@@ -0,0 +1,19 @@
+require 'httparty'
+
+class Kele
+  include HTTParty
+  base_uri 'https://www.bloc.io/api/v1'
+
+  def initialize(email, password)
+    response = self.class.post('/sessions', body: { "email": email, "password": password })
+
+    if @auth == nil
+      'Invalid Credentials'
+    else
+      'Welcome'
+    end
+
+    @auth = response['auth_token']
+
+  end
+end
