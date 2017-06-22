1c8f6e6e7159e9234dbaaa27d5b339f1d91ef479
Ryan McBurney <rrmcburn@gmail.com>
2017-06-21 20:18:27 -0400

    Added get_me

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -7,11 +8,19 @@ class Kele
     base_url = 'https://www.bloc.io/api/v1'
     @auth_token = self.class.post('https://www.bloc.io/api/v1/sessions', body: {"email": email, "password": password})
 
-    if @auth_token == nil
+    if @auth_token == '' || nil
       puts 'Invalid Credentials'
     else
       puts 'Welcome'
     end
+
+  end
+
+  def get_me
+
+  response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+  @user_data = JSON.parse(response.body)
+  @user_data
 
   end
 
